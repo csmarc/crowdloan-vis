b7a92f1a7222366b86a98acc1c5cdeb4cb16d8ed
csmarc <marton.csernai@gmail.com>
2021-08-30 17:03:36 +0200

    add first working kusama polling

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,28 @@
-import './styles.css';
 import React from 'react';
 import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
+import { ApiPromise, WsProvider } from '@polkadot/api';
+
+async function main() {
+  // Construct
+  const wsProvider = new WsProvider('wss://kusama-rpc.polkadot.io');
+  const api = await ApiPromise.create({ provider: wsProvider });
+
+  console.log(api.genesisHash.toHex());
+
+  const query = await api.query.crowdloan.funds.entries();
+  // query.forEach(([key, account]) => {
+  //   console.log(`${key.args}; ${account.unwrap().raised.toString()}`);
+  // });
+  const data2 = query.map(([key, account]) => {
+    const id = key.args.toString();
+    const raised = +account.unwrap().raised.toString() / Math.pow(10, 12);
+    const cap = +account.unwrap().cap.toString() / Math.pow(10, 12);
+    return { id, raised, cap };
+  });
+  console.log(data2);
+  // return data2;
+}
+main().finally();
 
 const data = [
   {
@@ -47,7 +69,7 @@ const data = [
   },
 ];
 
-export default function App() {
+function App() {
   return (
     <div className="App">
       <BarChart
@@ -66,9 +88,11 @@ export default function App() {
         <YAxis />
         <Tooltip />
         <Legend />
-        <Bar dataKey="pv" stackId="a" fill="#8884d8" />
-        <Bar dataKey="uv" stackId="a" fill="#82ca9d" />
+        <Bar dataKey="raised" stackId="a" fill="#8884d8" />
+        <Bar dataKey="remaining" stackId="a" fill="#82ca9d" />
       </BarChart>
     </div>
   );
 }
+
+export default App;
